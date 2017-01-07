e9633a2efdc2795b04a36829381d3e15021d61ae
Sakeeb Hossain <sakeeb.hossain1@gmail.com>
2017-01-07 18:42:54 -0500

    this is lame

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 //Middleware
 app.listen(3000)
 
 app.get('/', function(req, res){
 res.send('Hello World');
 });
-
-//app.listen();
-//console.log('Express server started on port %s', app.address().port);
