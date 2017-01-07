339acf6cdd5889115e6c96b036eb413f288c608f
ShekMaha <abhimaha12@gmail.com>
2017-01-07 18:49:28 -0500

    adding vision api

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var app = express();
 //Middleware
 app.listen(3000);
 
+var Vision = require('@google-cloud/vision');
+
+// By default, the client will authenticate using the service account file
+// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// the project specified by the GCLOUD_PROJECT environment variable. See
+// https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
+
+// Instantiate a vision client
+var vision = Vision();
+
 app.get('/', function(req, res){
 res.send('Hello World');
 });
@@ -25,9 +35,3 @@ login({
 		res.send('success');
 	});
 });
-
-
-
-
-//app.listen();
-//console.log('Express server started on port %s', app.address().port);
