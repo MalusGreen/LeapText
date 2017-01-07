57d2350fdd7d92fe481f5776c3b9bcea7c40534f
ShekMaha <abhimaha12@gmail.com>
2017-01-07 18:33:29 -0500

    updated server with incoming message handling

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var app = express();
 //Middleware
-app.listen(3000)
+app.listen(3000);
 
 app.get('/', function(req, res){
 res.send('Hello World');
 });
+
+var login = require('facebook-chat-api');
+var myThreadID = 100004714058918;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+login({
+	email: "leapsurface@gmail.com",
+	password: "uncommon"
+	},
+	function processMessage(err, api){
+	api.sendMessage('helloWorld', myThreadID);
+	app.post('/', function(req, res){
+		console.log(req.body);
+		res.send('success');
+	});
+});
+
+
+
 
 //app.listen();
 //console.log('Express server started on port %s', app.address().port);
