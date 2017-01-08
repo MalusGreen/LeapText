a5a1daed35952239f313d49a154a0a7862d927c3
ShekMaha <abhimaha12@gmail.com>
2017-01-07 21:45:00 -0500

    did a bunch of changes

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
+"use strict"
+
 var express = require('express');
 var app = express();
 var http = require('http');
 //Middleware
 app.listen(3000);
 
-var Vision = require('@google-cloud/vision');
-
-// By default, the client will authenticate using the service account file
-// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
-// the project specified by the GCLOUD_PROJECT environment variable. See
-// https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
-
+//var Vision = require('@google-cloud/vision');
 // Instantiate a vision client
-var vision = Vision();
+//var vision = Vision();
 
 app.get('/', function(req, res){
 res.send('Hello World');
@@ -31,10 +27,34 @@ login({
 	password: "uncommon"
 	},
 	function processMessage(err, api){
-	api.sendMessage('helloWorld', myThreadID);
+	//api.sendMessage('helloWorld', myThreadID);
 	app.post('/', function(req, res){
 		console.log(req.body);
 		res.send('success');
 	});
 });
+
+var google = require('googleapis');
+
+const vision = require('node-cloud-vision-api')
+
+// init with auth
+vision.init({auth: 'AIzaSyAjZcLeiAphMon-xzpVU-bBvvg3uPRPgw0'})
+
+// construct parameters
+const req = new vision.Request({
+  image: new vision.Image({
+  	url: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/AMS_Euler_sample_text.svg'}),
+  features: [
+  	new vision.Feature('TEXT_DETECTION', 10)
+  ]
+})
+
+// send single request
+vision.annotate(req).then((res) => {
+  // handling response
+  console.log(JSON.stringify(res.responses[0].textAnnotations[1].description))
+}, (e) => {
+  console.log('Error: ', e)
+})
 
