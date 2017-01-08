55b8aa8575623d1f573832e2180c2b1567aad5cc
ShekMaha <abhimaha12@gmail.com>
2017-01-08 08:38:18 -0500

    fixed bugs?

--- server.js
+++ server.js
@@ -15,10 +15,18 @@ res.send('Hello World');
 
 var login = require('facebook-chat-api');
 var myThreadID = 100004714058918;
+var Twitter = require('twitter');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+var client = new Twitter({
+  consumer_key: 'L5y0SwyJrIhoAwuTWUNnuDt5u',
+  consumer_secret: 'sVrzr3e7C3RaW672lHk6nYUXYmSAl1HiM1A6ox3mRn4OAiKyV3',
+  access_token_key: '817995136353390592-6ud0vTD64ZdYrt1v1EB8M1RkemOQPiS',
+  access_token_secret: 'DoxOnvljQq4Op7a4A2UQMIZW4NCAa6vRubzwC9bzbcNz7'
+});
 
 login({
 	email: "leapsurface@gmail.com",
@@ -29,6 +37,7 @@ login({
 	app.post('/', function(req, res){
 		var givenUrl = req.body.imgurLink;
 		var swipe_left = req.body.direction;
+		console.log(givenUrl);
 		//console.log(req.body);
 		// construct parameters
 const my_req = new vision.Request({
@@ -59,7 +68,7 @@ vision.annotate(my_req).then((res) => {
   	if (swipe_left){
 		api.sendMessage('you messed up dawg', myThreadID);
   	}else{
-  		client.post('statuses/update', {status: actualMessage},  function(error, tweet, response) {
+  		client.post('statuses/update', {status: 'you messed up dawg'},  function(error, tweet, response) {
   			if(error) throw error;
   			console.log(tweet);  // Tweet body. 
   			console.log(response);  // Raw response object. 
@@ -81,12 +90,7 @@ const vision = require('node-cloud-vision-api')
 // init with auth
 vision.init({auth: 'AIzaSyAjZcLeiAphMon-xzpVU-bBvvg3uPRPgw0'})
 
-var Twitter = require('twitter');
+
  
-var client = new Twitter({
-  consumer_key: 'L5y0SwyJrIhoAwuTWUNnuDt5u',
-  consumer_secret: 'sVrzr3e7C3RaW672lHk6nYUXYmSAl1HiM1A6ox3mRn4OAiKyV3',
-  access_token_key: '817995136353390592-6ud0vTD64ZdYrt1v1EB8M1RkemOQPiS',
-  access_token_secret: 'DoxOnvljQq4Op7a4A2UQMIZW4NCAa6vRubzwC9bzbcNz7'
-});
+
 
